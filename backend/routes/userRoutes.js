cf5b14b4999e42d3a94bb64c613117d148204fde
David Jacovic <jackodavid64@gmail.com>
2025-05-24 15:04:34 +0200

    PAR-126 Registracija in login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,30 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController.js');
-
 /*
- * GET
- */
 router.get('/', userController.list);
-
-/*
- * GET
- */
 router.get('/:id', userController.show);
-
-/*
- * POST
- */
-router.post('/', userController.create);
-
-/*
- * PUT
- */
 router.put('/:id', userController.update);
-
-/*
- * DELETE
- */
 router.delete('/:id', userController.remove);
+*/
 
+router.get('/register', userController.showRegister);
+router.get('/login', userController.showLogin); 
+
+router.post('/', userController.create);
+router.post('/login', userController.login);
 module.exports = router;
---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,19 @@
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../userContext';
+
+function Logout() {
+    const { setUserContext } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        setUserContext(null);
+        navigate('/login');
+    }, [navigate, setUserContext]);
+
+    return null;
+}
+
+export default Logout;
