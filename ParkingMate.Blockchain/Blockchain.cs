9b9590678f8552a50bf8e6294118199951cfd4cb
David Jacovic <jackodavid64@gmail.com>
2025-12-27 22:02:03 +0100

    PAR-344 Implementirati genesis blok (hash prejšnjega bloka = 0)

--- ParkingMate.Blockchain/Blockchain.cs
+++ ParkingMate.Blockchain/Blockchain.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System;
+
 
 namespace ParkingMate.Blockchain
 {
@@ -9,8 +11,25 @@ namespace ParkingMate.Blockchain
         public Blockchain()
         {
             chain = new List<Block>();
+            chain.Add(CreateGenesisBlock());
         }
 
         public IReadOnlyList<Block> Chain => chain;
+
+        private Block CreateGenesisBlock()
+        {
+            var genesis = new Block(
+                index: 0,
+                data: "Genesis Block",
+                timestamp: DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                previousHash: "0",
+                difficulty: 1,
+                nonce: 0
+            );
+
+            genesis.Hash = genesis.CalculateHash();
+            return genesis;
+        }
+
     }
 }
