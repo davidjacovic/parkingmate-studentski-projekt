5753730687e2e09e86678234950ba9b87e8e494a
sara.veskovic <sara.veskovic@student.um.si>
2025-12-28 18:01:43 +0100

    PAR-380-Pošiljanje-slike-parkirišča-na-strežnik

--- pora/ParkingMate/app/src/main/java/com/example/parkingmate/IntroActivity.kt
+++ pora/ParkingMate/app/src/main/java/com/example/parkingmate/IntroActivity.kt
@@ -1,28 +1,22 @@
 package com.example.parkingmate
+
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import android.widget.TextView
-import com.example.parkingmate.R
 
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var tvTitle: TextView
-    private lateinit var btnLogin: Button
-    private lateinit var btnRegister: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
         tvTitle = findViewById(R.id.tvTitle)
-        btnLogin = findViewById(R.id.btnLogin)
-        btnRegister = findViewById(R.id.btnRegister)
 
         val fadeIn = AlphaAnimation(0f, 1f)
         fadeIn.duration = 2000
@@ -30,20 +24,8 @@ class IntroActivity : AppCompatActivity() {
         tvTitle.startAnimation(fadeIn)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            btnLogin.visibility = View.VISIBLE
-            btnRegister.visibility = View.VISIBLE
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }, 2500)
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        btnRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
 }
