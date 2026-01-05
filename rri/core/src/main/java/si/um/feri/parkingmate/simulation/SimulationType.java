69b50b040cf4375d98c6c40f83ba56b4267c38c8
sara.veskovic <sara.veskovic@student.um.si>
2026-01-05 14:21:50 +0100

    PAR-436-Posebni-dogodki-koncerti-tekme

---
+++ rri/core/src/main/java/si/um/feri/parkingmate/simulation/SimulationType.java
@@ -0,0 +1,11 @@
+package si.um.feri.parkingmate.simulation;
+
+// Enum za tipove simulacija
+public enum SimulationType {
+    NONE,
+    CONCERT,
+    MATCH,
+    MORNING,
+    DAYTIME,
+    EVENING
+}
